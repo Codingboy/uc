8d263970ef7417bf590b0bc12aacd784474cc5b8
Daniel Tkocz <dtk@dtk-G31M-ES2L.(none)>
2013-10-21 11:14:39 +0200

    ez3.h edited

--- ez3.h
+++ ez3.h
@@ -12,14 +12,15 @@ typedef struct
 /**
  * Initialises an EZ3 sensor.
  * When this function is called, the range between EZ3 sensor and objects in front of it shall be minimum 17.78 cm, optimum 35.56 cm or greater.
+ * @pre time.h is initialised
  */
 void initEz3(Ez3* ez3, Gpio* rx, u8 portRx, u8 bitRx, Gpio* tx, u8 portTx, u8 bitTx);
 
 /**
  * Measures the range between EZ3 sensor and an object.
  * EZ3 sensor detects objects in between range from 15.24 up to 645 cm.
- * Resolution is 2.54 cm.
  * Objects nearer than 15.24 cm will be detected as if they were in a range of 15.24 cm.
+ * @return distance to object in cm or 0 if no object was detected
  */
 u16 measureEz3(Ez3* ez3);
 
