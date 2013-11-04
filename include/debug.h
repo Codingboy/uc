b997826a81b3ee45c11de2e5db81e6951d0448b7
Daniel Tkocz <dtk@dtk-G31M-ES2L.(none)>
2013-11-04 14:57:14 +0100

    specified blinking of debugled for each mode

--- include/debug.h
+++ include/debug.h
@@ -11,6 +11,18 @@ void initDebug(void);
 /**
  * Sets a debugmode.
  * Depending on the mode, the debug LED will blink other.
+state off on
+0 1900 100
+1 900 100
+2 400 100
+3 100 1900
+4 100 900
+5 100 400
+6 1000 1000
+7 500 500
+8 250 250
+9 100 100
+10 50 50
  */
 void setDebug(u8 debugLevel);
 
